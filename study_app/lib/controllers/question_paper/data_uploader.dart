6e1df95f660615ea9e637172d664b7e856320b04
jalala-pv <pvjalala@gmail.com>
2023-03-19 10:49:48 +0530

    aaa

--- study_app/lib/controllers/question_paper/data_uploader.dart
+++ study_app/lib/controllers/question_paper/data_uploader.dart
@@ -1,7 +1,11 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:study_app/models/question_paper_model.dart';
 
 class DataUploader extends GetxController {
   @override
@@ -11,9 +15,20 @@ class DataUploader extends GetxController {
   }
 
   Future<void> uploadData() async {
+  final firestore=FirebaseFirestore.instance; 
    final manifestContent=await DefaultAssetBundle.of(Get.context!).loadString('AssetManifest.json');
    final Map<String,dynamic>manifestMap=json.decode(manifestContent);
+   //load json file and print path
    final papersInAssets=manifestMap.keys.where((path) => path.startsWith('assets/DB/paper')&&path.contains('.json')).toList();
-   print(papersInAssets);
+   List<QuestionPaperModel>questionPapers=[];
+   for(var paper in papersInAssets){
+    //content in each db paper
+    String stringPaperContent=await rootBundle.loadString(paper);
+  questionPapers.add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+   }
+   //access items from the model
+   //print('Items number ${questionPapers[0].description}');
+   var batch=firestore.batch();
+
   }
 }
